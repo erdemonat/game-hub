3fed1278a47307f6c813a3c9a684c7a66fa8f034
erdemonat <onataydin22@gmail.com>
2025-03-07 16:39:32 +0300

    Implement dark mode

---
+++ src/components/ColorModeSwitch.tsx
@@ -0,0 +1,22 @@
+import { Icon, Switch } from "@chakra-ui/react";
+import { FaMoon, FaSun } from "react-icons/fa";
+import { useColorMode } from "./ui/color-mode";
+
+const ColorModeSwitch = () => {
+  const { toggleColorMode } = useColorMode();
+
+  return (
+    <Switch.Root colorPalette="blue" size="lg" onChange={toggleColorMode}>
+      <Switch.HiddenInput />
+      <Switch.Control>
+        <Switch.Thumb />
+        <Switch.Indicator fallback={<Icon as={FaMoon} color="gray.400" />}>
+          <Icon as={FaSun} color="yellow.400" />
+        </Switch.Indicator>
+      </Switch.Control>
+      <Switch.Label>Switch me</Switch.Label>
+    </Switch.Root>
+  );
+};
+
+export default ColorModeSwitch;
