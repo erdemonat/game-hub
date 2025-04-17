0dee159167152cf2a6dd06343bc65662235153cb
erdemonat <onataydin22@gmail.com>
2025-04-17 23:29:56 +0300

    Refactor- fetch genres with React Query

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,20 @@
 import { Provider } from "./components/ui/provider.tsx";
 import { StrictMode } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "/src/index.css";
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </Provider>
   </StrictMode>
 );
