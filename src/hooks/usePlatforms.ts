35e6555f59d88a4e205c9852c29745c125ec588e
erdemonat <onataydin22@gmail.com>
2025-04-23 21:16:49 +0300

    Refactor- fetch games with React Query & remove duplicate interfaces

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,9 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
-import { FetchResponse } from "./useData";
-import apiClient from "@/services/api-client";
+import apiClient, { FetchResponse } from "@/services/api-client";
 import platforms from "@/data/platforms";
 
-interface Platform {
+export interface Platform {
   id: number;
   name: string;
   slug: string;
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
 
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
 export default axios.create({
-    baseURL: 'https://api.rawg.io/api',
-    params: {
-        key: 'f7fd68258bc44dd2a3504a8c1d6d8426'
-    }
-})
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "f7fd68258bc44dd2a3504a8c1d6d8426",
+  },
+});
